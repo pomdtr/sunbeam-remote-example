ca7eafb94f8849d3a6b3483df62f79c6fb58d5c0
pomdtr <achille.lacoin@gmail.com>
2023-05-07 17:02:44 +0200

    add types

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
+import * as sunbeam from "https://deno.land/x/sunbeam@v1.0.0-rc.1/index.d.ts";
 
 serve(
   () =>
@@ -6,6 +7,7 @@ serve(
       JSON.stringify({
         type: "detail",
         preview: { text: "Hello from Deno Deploy!" },
-      })
+        actions: [{ type: "copy", text: "Hello from Deno Deploy!" }],
+      } as sunbeam.Detail)
     )
 );
