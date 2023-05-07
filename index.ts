c581ddb1822bc3f6bee55c7967e1c827d0e93936
pomdtr <achille.lacoin@gmail.com>
2023-05-07 12:11:35 +0200

    add remote extension

---
+++ index.ts
@@ -0,0 +1,8 @@
+// simple_api.js
+
+import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
+
+serve(
+  () =>
+    new Response(JSON.stringify({ type: "detail", preview: { text: "yolo" } }))
+);
